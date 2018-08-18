5a714a12dd210e80fe9847f2aa80c24a89b64c81
Jonathan <j.olaleye.o@gmail.com>
2018-08-18 11:28:12 -0500

    Add js demo

---
+++ demos/js.js
@@ -0,0 +1,42 @@
+class Banana {
+  constructor(goodOrNah) {
+    this.quality = goodOrNah;
+  }
+
+  eat() {
+    // omnomomnomom
+    const om = 'om nom';
+  }
+
+  static peelTime(length) {
+    const magicNumber = 123;
+    return length * magicNumber;
+  }
+}
+
+const banana = new Banana(10);
+banana.eat();
+console.log(Banana.peelTime(100));
+
+function makeBanana(num) {}
+
+for (let i = 0; i < 10; i++) {
+  makeBanana();
+}
+
+if (true) {}
+while (true) {}
+
+switch (fruit) {
+  case 'banana':
+    break;
+  default:
+    break;
+}
+
+const asyncFetchBananas = async () => {
+  let bananas = await findBananas();
+
+  const specialBanana = new Banana(11);
+  bananas.push(specialBanana);
+}
