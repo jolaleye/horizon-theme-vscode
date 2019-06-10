f1a4f48af1275bc8fb756825b7a4a12b77112bc1
jolaleye <j.olaleye.o@gmail.com>
2019-06-10 13:11:02 -0500

    Add theme src files, finalize build script

--- src/build.js
+++ src/build.js
@@ -3,10 +3,10 @@ const mustache = require('mustache');
 
 const colors = require('./colors.json');
 const template = JSON.stringify(require(`${__dirname}/template.json`));
-const themeNames = ['horizon'];
+const themeNames = ['horizon', 'horizon-italic', 'horizon-bold'];
 
 for (themeName of themeNames) {
   const content = require(`${__dirname}/${themeName}.json`);
   const theme = mustache.render(template, { ...content, ...colors });
-  fs.writeFileSync(`${__dirname}/../themes/${themeName}2.json`, theme); // REMOVE 2s
+  fs.writeFileSync(`${__dirname}/../themes/${themeName}.json`, theme);
 }
