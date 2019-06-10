9bcbede330bb45558e69db7719e3a2d6b7cbdf9d
jolaleye <j.olaleye.o@gmail.com>
2019-06-10 18:43:09 -0500

    Move src content to src/dark

--- src/dark/build.js
+++ src/dark/build.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const mustache = require('mustache');
 
-const colors = require('./colors.json');
+const globals = require('./globals.json');
 const template = JSON.stringify(require(`${__dirname}/template.json`));
 const themeNames = ['horizon', 'horizon-italic', 'horizon-bold'];
 
 for (themeName of themeNames) {
   const content = require(`${__dirname}/${themeName}.json`);
-  const theme = mustache.render(template, { ...content, ...colors });
-  fs.writeFileSync(`${__dirname}/../themes/${themeName}.json`, theme);
+  const theme = mustache.render(template, { ...content, ...globals });
+  fs.writeFileSync(`${__dirname}/../../themes/${themeName}.json`, theme);
 }
